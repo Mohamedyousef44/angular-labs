4ce1a7fb0bbd868a5c4bed9c875615a37b006218
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-04-13 12:27:35 +0200

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputComponent } from './input/input.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    SliderComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
