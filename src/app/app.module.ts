5ffce7397baa0b07f5ccc79db21a71e93d037576
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-04-13 12:33:33 +0200

    build button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { InputComponent } from './input/input.component';
+import { InputComponent } from './components/input/input.component';
 import { SliderComponent } from './components/slider/slider.component';
 import { ButtonComponent } from './components/button/button.component';
 
