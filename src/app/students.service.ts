aab164ae49fb220eafe37fded5bdb94c37eed02c
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-04-18 14:24:43 +0200

    create service module

--- src/app/students.service.ts
+++ src/app/students.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentsService {
 
-  constructor() { }
+  constructor(private client: HttpClient) { }
+
+  private Base_URL = "http://localhost:3000/students";
+
+  all(){
+    return this.client.get(this.Base_URL)
+  }
+
+  getOneById(id: number){
+    return this.client.get(this.Base_URL+'/'+id)
+  }
+
+  create(student: any){
+    return this.client.post(this.Base_URL , student)
+  }
+
+  update(student: any){
+    return this.client.put(this.Base_URL , student)
+  }
+
+  delete(student: any){
+    return this.client.delete(this.Base_URL , student)
+  }
 }
